be26290aa356a3159ad986093b4a93afba6f5bcb
Giraldo, Sofia <sofia.giraldohincapie@nielseniq.com>
2023-10-29 20:39:40 -0500

    refactor(main): Agregar un libro y el progreso

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 # Book Tracker 
 
-### Palette:
+- [Book Tracker](#book-tracker)
+  - [Description](#description)
+  - [Page Settings](#page-settings)
+    - [1. Palette:](#1-palette)
+
+
+## Description
+Monitor the status of the books in your current reading queue and incorporate the titles you intend to read in the future into your reading list
+
+## Page Settings
+
+### 1. Palette:
+
 - Check the complete color palette [here](https://mycolor.space/?hex=%23990011&sub=1)
 ![palette](assets/img/Screenshot_3.png)
-
-
-
-
--- js/main.js
+++ js/main.js
@@ -1,41 +1,95 @@
-continuar = false; 
+class nuevoLibro {
+    constructor(nombre, autor, numeroTotalPaginas, numeroPaginasLeidas) {
+        this.nombre = nombre;
+        this.autor = autor; 
+        this.numeroTotalPaginas = numeroTotalPaginas;
+        this.numeroPaginasLeidas = numeroPaginasLeidas;
+        this.progreso = Math.round((numeroPaginasLeidas/numeroTotalPaginas)*100);
+    }
+}
 
+//! Variables inicializadoras
+let continuar = false; 
+const librosTrackeados = [];
+
+//! Declaracion de la funcion para empezar
+let boton_empezar_proceso = document.getElementById("empezar");
 function start_tracking() {
     continuar = true;
     while (continuar == true) {
-        reconocer_libro()
+        let nuevo_libro = prompt("Ingresa el nombre del libro");
+            if(nuevo_libro == null) {
+                alert("No entendimos tu respuesta. Intentalo de nuevo mas tarde.")
+                continuar = false;
+            } else {
+                let autor_libro = prompt("Ingresa el nombre del autor");
+                if(autor_libro == null) {
+                    alert("No entendimos tu respuesta. Intentalo de nuevo mas tarde.")
+                    continuar = false;
+                } else {
+                    let total_paginas = Number(prompt("Ingresa el total de paginas que el libro contiene.\n\nNota: debes poner unicamente el numero de paginas usando enteros (ej: 500)"));
+                    let total_paginas_leidas = Number(prompt("Cuantas paginas has leido hasta ahora? \n\nNota: debes poner unicamente el numero de paginas usando enteros (ej: 500)"));
+                    reconocer_libro(nuevo_libro, autor_libro, total_paginas, total_paginas_leidas);
+                }
+            }
     }
+};
+boton_empezar_proceso.addEventListener("click", start_tracking);
+
+
+
+//! Funcion para agregar un nuevo libro al array
+function reconocer_libro(nuevo_libro, autor_libro, total_paginas, total_paginas_leidas) {
+    librosTrackeados.push(new nuevoLibro(nuevo_libro, autor_libro, total_paginas, total_paginas_leidas));
+    continuar = confirm("Quieres continuar registrando progreso?");
+    if (continuar==false) {
+        visualizar_libros(librosTrackeados)
+    } 
 }
 
-function reconocer_libro() {
-    nuevo_libro = prompt("Ingresa el nombre del libro");
-    total_paginas = Number(prompt("Ingresa el total de paginas que el libro contiene.\n\nNota: debes poner unicamente el numero de paginas usando enteros (ej: 500)"));
-    total_paginas_leidas = Number(prompt("Cuantas paginas has leido hasta ahora? \n\nNota: debes poner unicamente el numero de paginas usando enteros (ej: 500)"));
-    calcular_progreso();
-    continuar = confirm(mensaje + " Quieres continuar registrando progreso?");
-}
 
-function calcular_progreso() {
-    progreso = (total_paginas_leidas/total_paginas)*100
-    mensaje_usuario(progreso);
-    return mensaje
-}
+//! Funcion para imprimir todos los libros agregados o por los que el usuario desee filtrar
+const visualizar_libros = (librosTrackeados) => {
+    console.log(librosTrackeados);
+    let visualizar = confirm("Quieres visualizar los libros agregados?");
+    if (visualizar === true) {
+        let opciones_visualizacion = Number(prompt("Tenemos varias opciones de visualizacion elige la que quieras usar hoy: \n1). Ver todos los libros agregados \n2). Ver libros comenzados recientemente (menos del 10% de progreso) \n3). Ver libros con entre 10% y 49% de progreso  \n4). Ver libros con 50% de progreso o mas \n5). Ver libros terminados" ));
+        let libros_filtrados = [];
+            if(opciones_visualizacion === 1) {
+                libros_filtrados = librosTrackeados;
+            } else if (opciones_visualizacion === 2) {
+                libros_filtrados = librosTrackeados.filter((libros) => libros.progreso <= 10)
+                console.log(libros_filtrados);
+            } else if (opciones_visualizacion === 3) {
+                libros_filtrados = librosTrackeados.filter((libros) => libros.progreso > 10 && libros.progreso < 50)
+            } else if (opciones_visualizacion === 4) {
+                libros_filtrados = librosTrackeados.filter((libros) => libros.progreso >= 50  && libros.progreso < 100)
+            } else if (opciones_visualizacion === 5) {
+                    libros_filtrados = librosTrackeados.filter((libros) => libros.progreso == 100)
+            } else {
+                opciones_visualizacion = prompt("Por favor selecciona una opcion valida")
+            }
+            console.log(opciones_visualizacion);
+            console.log(libros_filtrados);
+            console.log(libros_filtrados.length);
 
-
-function mensaje_usuario(progreso) {
-    if(progreso < 10){
-        mensaje = `Llevas ${progreso}% del libro ${nuevo_libro}. Apenas vas comenzando, Animo!`;
-    } else if (progreso > 10 && progreso < 50 ){
-        mensaje = `Llevas ${progreso}% del libro ${nuevo_libro}. Vas muy bien. Continua asi!`;
-    } else if (progreso > 50 && progreso < 90) {
-        mensaje = `Llevas ${progreso}% del libro ${nuevo_libro}. Ya pasaste la mitad. Eres impresionante!`;
-    } else if (progreso > 90 && progreso < 100) {
-        mensaje = `Llevas ${progreso}% del libro ${nuevo_libro}. Estas a punto de terminar. No te falta nada!`;
-    } else if (progreso == 100) {
-        mensaje = `Terminaste ${nuevo_libro}!!!.`;
-    } else {
-        mensaje = `No pudimos calcular tu progreso. Intentalo de nuevo mas tarde.`;
-        continuar = false
+        
+        let mensaje_final = "Tus libros son:";
+        if (libros_filtrados.length == 0) {
+            alert("No existe nungun libro en la categoria seleccionada.")
+        } else {
+            libros_filtrados.forEach((libro) => {
+                let mensaje = `
+                Nombre: ${libro.nombre}
+                Autor: ${libro.autor}
+                Progreso: ${libro.progreso}
+                `
+                mensaje_final += mensaje
+            });
+            alert(mensaje_final);
+        }
+    } else { 
+        alert("Gracias por visitarnos! Continua leyendo!");
     }
-} 
+};
 
